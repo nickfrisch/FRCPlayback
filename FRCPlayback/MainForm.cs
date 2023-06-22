ef411e483646e456eb0bab8a60921e8b086f9cfc
Nick Frisch <nfrisch3@outlook.com>
2023-06-22 18:11:21 -0400

    Setup basic C# project

---
+++ FRCPlayback/MainForm.cs
@@ -0,0 +1,15 @@
+namespace FRCPlayback
+{
+    public partial class FRCPlayback : Form
+    {
+        public FRCPlayback()
+        {
+            InitializeComponent();
+        }
+
+        public void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
