ff2c431b92bf9f1c2ebe594c57d83e3f656aeacf
Nick Frisch <nfrisch3@outlook.com>
2023-06-24 19:34:40 -0400

    Start on progress bar and downloading
    Need to use some library for youtube

--- FRC Playback/MainWindow.xaml.cs
+++ FRC Playback/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -35,12 +37,33 @@ namespace FRC_Playback
             Configuration.Default.BasePath = "https://www.thebluealliance.com/api/v3";
             Configuration.Default.ApiKey.Add("X-TBA-Auth-Key", "Ow8SODFIWtXsxk9KbI0tDPooPibzYcyzhSiJ9saUWQXGIf5ENQ8pwnb1Bi2gwxhj");
 
-            var apiInstance = new EventApi(Configuration.Default);
-            List<EventSimple> eventsSimple = apiInstance.GetEventsByYearSimple(DateTime.Now.Year);
-            foreach (EventSimple simpleEvent in eventsSimple)
+            var apiInstance = new MatchApi(Configuration.Default);
+            Match match = apiInstance.GetMatch("2023mijac_qm50");
+            Debug.WriteLine(match.ToString());
+            Debug.WriteLine(match.Videos[0].ToString());
+
+            using (WebClient wc = new WebClient())
             {
-                events.Add(simpleEvent.Key);
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileAsync(
+                    // Param1 = Link of file
+                    new System.Uri(string.Format("https://www.youtube.com/watch?v={0}", match.Videos[0].Key)),
+                    // Param2 = Path to save
+                    "C:\\GitRepos\\FRCPlayback"
+                );
             }
+        }
+
+        // Event to track the progress
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            VideoDownloadProgress.Value = e.ProgressPercentage;
+        }
+
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
         }
     }
 }
