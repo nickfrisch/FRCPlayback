50dbfba86efd76e9135302aed3af846626a95c39
Wyatt <26737@oxfordwildcats.org>
2023-06-25 13:40:07 -0400

    Json Data Puller and Storing
    Needs closing functions and manual rewrite

--- FRC Playback/TBAUtils/TBABrowser.xaml.cs
+++ FRC Playback/TBAUtils/TBABrowser.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,6 +12,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -34,18 +38,33 @@ namespace FRC_Playback.TBAUtils
         EventApi eventAPIInstance;
         DistrictApi districtAPIInstance;
 
+        private class TreeHeaderObj
+        {
+            string header;
+            string key;
+
+            public TreeHeaderObj(string header, string key)
+            {
+                this.header = header;
+                this.key = key;
+            }
+
+            public override string ToString()
+            {
+                return this.header;
+            }
+
+            public string GetKey()
+            {
+                return this.key;
+            }
+        }
+
         public TBABrowser()
         {
             InitializeComponent();
 
-            tbaDataCachePath = System.IO.Path.GetTempPath() + "FRCPlayback_TBADataCache.json";
-            CachePath.Text = tbaDataCachePath;
-
-            FileStream fileStream;
-            if (!File.Exists(tbaDataCachePath))
-                fileStream = File.Create(tbaDataCachePath);
-            else
-                fileStream = new FileStream(tbaDataCachePath, FileMode.Open, FileAccess.ReadWrite);
+            //////////////////////Blue Alliance Client Setup//////////////////////
 
             Configuration.Default.BasePath = "https://www.thebluealliance.com/api/v3";
             Configuration.Default.ApiKey.Add("X-TBA-Auth-Key", "Ow8SODFIWtXsxk9KbI0tDPooPibzYcyzhSiJ9saUWQXGIf5ENQ8pwnb1Bi2gwxhj");
@@ -53,89 +72,301 @@ namespace FRC_Playback.TBAUtils
             eventAPIInstance = new EventApi(Configuration.Default);
             districtAPIInstance = new DistrictApi(Configuration.Default);
 
-            /*
-            try
-            {
-                tbaData = JsonSerializer.Deserialize<TBACache>(fileStream);
-                fileStream.Close();
-            }
-            catch (JsonException e)
-            {
-                fileStream.Close();
-                pushDataInCache();
-            }*/
+            //////////////////////TBA Cache Reader//////////////////////
 
-            fileStream.Close();
-            buildBasicCacheIndex();
+            // Get the file path for the TBA Cache Data
+            tbaDataCachePath = System.IO.Path.GetTempPath() + "FRCPlayback_TBADataCache.json";
+
+            // Open the file stream if the file exists otherwise create a new file
+            FileStream fileStream;
+            if (!File.Exists(tbaDataCachePath))
+                fileStream = File.Create(tbaDataCachePath);
+            else
+                fileStream = new FileStream(tbaDataCachePath, FileMode.Open, FileAccess.ReadWrite);
+
+            // Start the Deserialization of the JSON object using a Steam to conserve memory
+            var serializer = new Newtonsoft.Json.JsonSerializer();
+            using (var textReader = new StreamReader(fileStream))
+            using (var jsonReader = new JsonTextReader(textReader))
+            {
+                tbaData = serializer.Deserialize<TBACache>(jsonReader);
+            }
+
+            fileStream.Close(); // Close the file stream because it is no longer needed.
+
+            buildBasicCache(false);
 
             TBASearchBar.Items.Clear();
 
-            /*
-
-            List<DistrictList> districts = districtAPIInstance.GetDistrictsByYear(DateTime.Now.Year);
-            foreach (DistrictList district in districts)
+            foreach (YearCache year in tbaData.years)
             {
-                TreeViewItem districtItem = new TreeViewItem();
-                districtItem.Header = district.DisplayName;
-                districtItem.AllowDrop = true;
-                districtItem.Expanded += new RoutedEventHandler((sender, args) =>
-                {
-                    List<EventSimple> events = districtAPIInstance.GetDistrictEventsSimple(district.Key);
-                    
-                    foreach (EventSimple simpleEvent in events)
-                    {
-                        TreeViewItem eventItem = new TreeViewItem();
-                        eventItem.Header = simpleEvent.Name;
-                        eventItem.AllowDrop = true;
-                        eventItem.Expanded += new RoutedEventHandler((sender, args) =>
-                        {
-                            List<Match> matches = eventAPIInstance.GetEventMatches(simpleEvent.Key);
-
-                            foreach (Match match in matches)
-                            {
-                                TreeViewItem matchItem = new TreeViewItem();
-                                matchItem.Header = match.Key;
-                                matchItem.AllowDrop = true;
-                                matchItem.Selected += new RoutedEventHandler((sender, args) =>
-                                {
-                                    RedAllianceTeam1Number.Text = match.Alliances.Red.TeamKeys[0].Substring(3);
-                                    RedAllianceTeam2Number.Text = match.Alliances.Red.TeamKeys[1].Substring(3);
-                                    RedAllianceTeam3Number.Text = match.Alliances.Red.TeamKeys[2].Substring(3);
-
-                                    BlueAllianceTeam1Number.Text = match.Alliances.Blue.TeamKeys[0].Substring(3);
-                                    BlueAllianceTeam2Number.Text = match.Alliances.Blue.TeamKeys[1].Substring(3);
-                                    BlueAllianceTeam3Number.Text = match.Alliances.Blue.TeamKeys[2].Substring(3);
-
-                                    TBASearchBar.Text = match.Key;
-                                });
-
-                                eventItem.Items.Add(matchItem);
-                            }
-                        });
-
-                        districtItem.Items.Add(eventItem);
-                    }
+                TreeViewItem yearItem = new TreeViewItem();
+                yearItem.Header = year.year.ToString();
+                yearItem.AllowDrop = true;
+                yearItem.Expanded += new RoutedEventHandler((sender, args) => {
+                    if (sender == TBATreeView.SelectedItem)
+                        onYearItemSelected(year, yearItem);
                 });
 
-                TBATreeView.Items.Add(districtItem);
-            } */
+                TBATreeView.Items.Insert(0, yearItem);
+            }
         }
 
-        void buildBasicCacheIndex()
+        void onYearItemSelected(YearCache year, TreeViewItem yearItem)
         {
-            List<DistrictList> districts = districtAPIInstance.GetDistrictsByYear(DateTime.Now.Year);
+            if (isRewriteRequired(year.lastUpdateTime, year.year, year.districts.Count))
+            {
+                buildBasicDistrictCache(year.year);
+                writeOutCache();
+            }
+
+            yearItem.Items.Clear();
+
+            foreach (DistrictCache district in year.districts)
+            {
+                TreeViewItem districtItem = new TreeViewItem();
+                districtItem.Header = new TreeHeaderObj(district.districtName, district.districtKey);
+                districtItem.AllowDrop = true;
+                districtItem.Expanded += new RoutedEventHandler((sender, args) => {
+                    if (sender == TBATreeView.SelectedItem)
+                        onDistrictItemSelected(year, district, districtItem);
+                });
+
+                yearItem.Items.Add(districtItem);
+            }
+        }
+
+        void onDistrictItemSelected(YearCache year, DistrictCache district, TreeViewItem districtItem)
+        {
+            //MessageBox.Show("Selected Item " + TBATreeView.SelectedItem.ToString());
+            //MessageBox.Show("District Item Selected");
+            if (isRewriteRequired(district.lastUpdateTime, year.year, district.events.Count))
+            {
+                buildBasicEventCache(year.year, district.districtKey, district.districtName);
+                writeOutCache();
+            }
+
+            districtItem.Items.Clear();
+
+            foreach (EventCache eventSimple in district.events)
+            {
+                TreeViewItem eventItem = new TreeViewItem();
+                eventItem.Header = new TreeHeaderObj(eventSimple.eventName, eventSimple.eventKey);
+                eventItem.AllowDrop = true;
+                eventItem.Expanded += new RoutedEventHandler((sender, args) =>
+                {
+                    if(sender == TBATreeView.SelectedItem)
+                        onEventItemSelected(year, district, eventSimple, eventItem);
+                });
+
+                districtItem.Items.Add(eventItem);
+            }
+        }
+
+        void onEventItemSelected(YearCache year, DistrictCache district, EventCache eventSimple, TreeViewItem eventItem)
+        {
+            if (isRewriteRequired(eventSimple.lastUpdateTime, year.year, eventSimple.matchCaches.Count))
+            {
+                buildBasicMatchCache(year.year, district.districtKey, eventSimple.eventKey, eventSimple.eventName);
+                writeOutCache();
+            }
+
+            eventItem.Items.Clear();
+
+            foreach (MatchCache match in eventSimple.matchCaches)
+            {
+                TreeViewItem matchItem = new TreeViewItem();
+                matchItem.Header = new TreeHeaderObj(match.matchKey, match.matchKey);
+                matchItem.AllowDrop = true;
+                matchItem.Selected += new RoutedEventHandler((sender, args) => {
+                    if (sender == TBATreeView.SelectedItem)
+                        onMatchItemSelected(match);
+                });
+
+                eventItem.Items.Add(matchItem);
+            }
+
+        }
+
+        void onMatchItemSelected(MatchCache match)
+        {
+            BlueAllianceTeam1Number.Text = match.blue1;
+            BlueAllianceTeam2Number.Text = match.blue2;
+            BlueAllianceTeam3Number.Text = match.blue3;
+            RedAllianceTeam1Number.Text = match.red1;
+            RedAllianceTeam2Number.Text = match.red2;
+            RedAllianceTeam3Number.Text = match.red3;
+        }
+
+        void buildBasicCache(bool forceAllUpdate)
+        {
+            //If there was no base input from the file then create the new structure
+            if(tbaData == null)
+                tbaData = new TBACache(DateTime.Now.ToLongDateString(), new List<YearCache>());
+
+            // For each year check for the basic data
+            for(int i = 2010; i < DateTime.Now.Year; i++)
+            {
+                // Set a flag and check every value in the json for a match
+                bool yearFlag = false;
+                foreach (YearCache year in tbaData.years)
+                {
+                    if(year.year == i)
+                        yearFlag = true;
+                }
+
+                // If there is no match or there is a force update pull the data from TBA
+                if (!yearFlag || forceAllUpdate)
+                    buildBasicDistrictCache(i);
+            }
+
+            // For the latest year repull the data no matter what
+            buildBasicDistrictCache(DateTime.Now.Year);
+
+            writeOutCache();
+        }
+
+        void buildBasicDistrictCache(int year)
+        {
+            List<DistrictList> districts = districtAPIInstance.GetDistrictsByYear(year);
+            string dateString = DateTime.Now.ToString();
 
             List<DistrictCache> districtCacheObj = new List<DistrictCache>();
             foreach (DistrictList district in districts)
             {
-                districtCacheObj.Add(new DistrictCache(district.Key, district.DisplayName, new EventCache[] { }));
+                districtCacheObj.Add(new DistrictCache(dateString, district.Key, district.DisplayName, new List<EventCache>()));
             }
 
-            tbaData = new TBACache("Now", new List<YearCache>());
-            tbaData.years.Add(new YearCache(DateTime.Now.Year, districtCacheObj));
+            int yearIdx = findYearIndex(year);
 
+            if (yearIdx >= 0)
+            {
+                YearCache yearItem = tbaData.years[yearIdx];// = new YearCache(dateString, year, districtCacheObj);
+                yearItem.lastUpdateTime = dateString;
+                yearItem.year = year;
+                yearItem.districts = districtCacheObj;
+            } 
+            else
+                tbaData.years.Add(new YearCache(dateString, year, districtCacheObj));
+        }
+
+        void buildBasicEventCache(int year, string districtKey, string districtName)
+        {
+            List<EventSimple> events = districtAPIInstance.GetDistrictEventsSimple(districtKey);
+            string dateString = DateTime.Now.ToString();
+
+            List<EventCache> eventCacheObj = new List<EventCache>();
+            foreach (EventSimple eventSimple in events)
+            {
+                eventCacheObj.Add(new EventCache(dateString, eventSimple.Key, eventSimple.Name, new List<MatchCache>()));
+            }
+
+            int yearIdx = findYearIndex(year);
+            int districtIdx = findDistrictIndex(yearIdx, districtKey);
+
+            if (districtIdx >= 0) {
+                DistrictCache districtItem = tbaData.years[yearIdx].districts[districtIdx];
+                districtItem.lastUpdateTime = dateString;
+                districtItem.districtKey = districtKey;
+                districtItem.districtName = districtName;
+                districtItem.events = eventCacheObj;
+            } else
+                tbaData.years[yearIdx].districts.Add(new DistrictCache(dateString, districtKey, districtName, eventCacheObj));
+        }
+
+        void buildBasicMatchCache(int year, string districtKey, string eventKey, string eventName)
+        {
+            List<MatchSimple> matches = eventAPIInstance.GetEventMatchesSimple(eventKey);
+            string dateString = DateTime.Now.ToString();
+
+            List<MatchCache> matchCacheObj = new List<MatchCache>();
+            foreach (MatchSimple match in matches)
+            {
+                matchCacheObj.Add(new MatchCache(dateString, match.Key,
+                    match.Alliances.Blue.TeamKeys[0].Substring(3),
+                    match.Alliances.Blue.TeamKeys[1].Substring(3),
+                    match.Alliances.Blue.TeamKeys[2].Substring(3),
+                    match.Alliances.Red.TeamKeys[0].Substring(3),
+                    match.Alliances.Red.TeamKeys[1].Substring(3),
+                    match.Alliances.Red.TeamKeys[2].Substring(3)));
+            }
+
+            int yearIdx = findYearIndex(year);
+            int districtIdx = findDistrictIndex(yearIdx, districtKey);
+            int eventIdx = findEventIndex(yearIdx, districtIdx, eventKey);
+
+            if (eventIdx >= 0)
+            {
+                EventCache eventItem = tbaData.years[yearIdx].districts[districtIdx].events[eventIdx]; // = new EventCache(dateString, eventKey, eventName, matchCacheObj);
+                eventItem.lastUpdateTime = dateString;
+                eventItem.eventKey = eventKey;
+                eventItem.eventName = eventName;
+                eventItem.matchCaches = matchCacheObj;
+            }
+            else
+                tbaData.years[yearIdx].districts[districtIdx].events.Add(new EventCache(dateString, eventKey, eventName, matchCacheObj));
+        }
+
+        public bool isRewriteRequired(string lastUpdateTime, int year, int itemCount)
+        {
+            TimeSpan timeFromLastUpdate = DateTime.Now.Subtract(DateTime.Parse(lastUpdateTime));
+
+            return timeFromLastUpdate > TimeSpan.FromDays(10) || (DateTime.Now.Year == year && timeFromLastUpdate > TimeSpan.FromHours(4)) || itemCount == 0;
+        }
+
+        public void writeOutCache()
+        {
+            // Write out the changes that were made
             File.WriteAllText(tbaDataCachePath, JsonConvert.SerializeObject(tbaData, Formatting.Indented));
         }
 
+        int findYearIndex(int year)
+        {
+            for(int i = 0; i < tbaData.years.Count; i++)
+            {
+                if (tbaData.years[i].year == year)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        int findDistrictIndex(int yearIdx, string districtKey)
+        {
+            for (int i = 0; i < tbaData.years[yearIdx].districts.Count; i++)
+            {
+                if (tbaData.years[yearIdx].districts[i].districtKey.Equals(districtKey))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        int findEventIndex(int yearIdx, int districtIdx, string eventKey)
+        {
+            for (int i = 0; i < tbaData.years[yearIdx].districts[districtIdx].events.Count; i++)
+            {
+                if (tbaData.years[yearIdx].districts[districtIdx].events[i].eventKey.Equals(eventKey))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private void TBABrowserOKButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        public string waitForResult()
+        {
+            return ((TreeHeaderObj) TBATreeView.SelectedItem).GetKey();
+        }
     }
 }
--- FRC Playback/TBAUtils/TBACacheObj.cs
+++ FRC Playback/TBAUtils/TBACacheObj.cs
@@ -9,34 +9,40 @@ namespace FRC_Playback.TBAUtils
 {
     public class TBACache
     {
-        public string lastUpdatedTime { get; set; }
+        public string lastUpdateTime { get; set; }
         public List<YearCache> years { get; set; }
 
         public TBACache(string lastUpdatedTime, List<YearCache> years)
         {
-            this.lastUpdatedTime = lastUpdatedTime;
+            this.lastUpdateTime = lastUpdatedTime;
             this.years = years;
         }
     }
 
     public class YearCache {
+
+        public string lastUpdateTime { get; set; }
         public int year { get; set; }
         public List<DistrictCache> districts { get; set; }
 
-        public YearCache(int year, List<DistrictCache> districts)
+        public YearCache(string lastUpdateTime, int year, List<DistrictCache> districts)
         {
+            this.lastUpdateTime = lastUpdateTime;
             this.year = year;
             this.districts = districts;
         }
     }
     public class DistrictCache
     {
+
+        public string lastUpdateTime { get; set; }
         public string districtKey { get; set; }
         public string districtName { get; set; }
-        public EventCache[] events { get; set; }
+        public List<EventCache> events { get; set; }
 
-        public DistrictCache(string districtKey, string districtName, EventCache[] events)
+        public DistrictCache(string lastUpdateTime, string districtKey, string districtName, List<EventCache> events)
         {
+            this.lastUpdateTime = lastUpdateTime;
             this.districtKey = districtKey;
             this.districtName = districtName;
             this.events = events;
@@ -45,12 +51,25 @@ namespace FRC_Playback.TBAUtils
 
     public class EventCache
     {
+
+        public string lastUpdateTime { get; set; }
         public string eventKey { get; set; }
-        public MatchCache[] matchCaches { get; set; }
+        public string eventName { get; set; } 
+        public List<MatchCache> matchCaches { get; set; }
+
+        public EventCache(string lastUpdateTime, string eventKey, string eventName, List<MatchCache> matchCaches)
+        {
+            this.lastUpdateTime = lastUpdateTime;
+            this.eventKey = eventKey;
+            this.eventName = eventName;
+            this.matchCaches = matchCaches;
+        }
     }
 
     public class MatchCache
     {
+        public string lastUpdateTime { get; set; }
+
         public string matchKey { get; set; }
 
         public string blue1 { get; set; }
@@ -60,5 +79,17 @@ namespace FRC_Playback.TBAUtils
         public string red1 { get; set; }
         public string red2 { get; set; }
         public string red3 { get; set; }
+
+        public MatchCache(string lastUpdateTime, string matchKey, string blue1, string blue2, string blue3, string red1, string red2, string red3)
+        {
+            this.lastUpdateTime = lastUpdateTime;
+            this.matchKey = matchKey;
+            this.blue1 = blue1;
+            this.blue2 = blue2;
+            this.blue3 = blue3;
+            this.red1 = red1;
+            this.red2 = red2;
+            this.red3 = red3;
+        }
     }
 }
