ee6bcfa571f92223d1184cc4d23c836c498cb5d4
Nick Frisch <nfrisch3@outlook.com>
2023-06-25 14:28:35 -0400

    Now downloads video

--- FRC Playback/MainWindow.xaml.cs
+++ FRC Playback/MainWindow.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,6 +20,9 @@ using System.Windows.Shapes;
 using TBAAPI.V3Client.Api;
 using TBAAPI.V3Client.Client;
 using TBAAPI.V3Client.Model;
+using YoutubeExplode;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
 
 namespace FRC_Playback
 {
@@ -33,37 +38,65 @@ namespace FRC_Playback
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
 
+        }
+        private async void DownloadVideo(string videoURL)
+        {
+            var youtube = new YoutubeClient();
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoURL);
+            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+
+            var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+
+            string videoFullPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\" + "videos", $"video.{streamInfo.Container}");
+
+            IProgress<double> progress = new Progress<double>(percent =>
+            {
+                VideoDownloadProgress.Value = percent;
+            });
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, videoFullPath, progress);
+
+            
+            /*
+            var client = new HttpClient();
+            long? totalByte = 0;
+            using (Stream output = File.OpenWrite(videoFullPath))
+            {
+                Debug.WriteLine("File: " + videoFullPath);
+                using (var request = new HttpRequestMessage(HttpMethod.Head, videoURL))
+                {
+                    totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
+
+                }
+                using (var input = await client.GetStreamAsync(videoURL))
+                {
+                    byte[] buffer = new byte[16 * 1024];
+                    int read;
+                    int totalRead = 0;
+                    Debug.WriteLine("Download Started");
+                    while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        output.Write(buffer, 0, read);
+                        totalRead += read;
+                        Debug.Write($"\rDownloading " + (double)totalRead/(double)totalByte +  "%...");
+                    }
+                    Debug.WriteLine("Download Complete");
+                }
+            }*/
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
             Configuration.Default.BasePath = "https://www.thebluealliance.com/api/v3";
             Configuration.Default.ApiKey.Add("X-TBA-Auth-Key", "Ow8SODFIWtXsxk9KbI0tDPooPibzYcyzhSiJ9saUWQXGIf5ENQ8pwnb1Bi2gwxhj");
 
             var apiInstance = new MatchApi(Configuration.Default);
             Match match = apiInstance.GetMatch("2023mijac_qm50");
-            Debug.WriteLine(match.ToString());
-            Debug.WriteLine(match.Videos[0].ToString());
 
-            using (WebClient wc = new WebClient())
-            {
-                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
-                wc.DownloadFileAsync(
-                    // Param1 = Link of file
-                    new System.Uri(string.Format("https://www.youtube.com/watch?v={0}", match.Videos[0].Key)),
-                    // Param2 = Path to save
-                    "C:\\GitRepos\\FRCPlayback"
-                );
-            }
-        }
-
-        // Event to track the progress
-        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        {
-            VideoDownloadProgress.Value = e.ProgressPercentage;
-        }
-
-
-        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-
+            DownloadVideo(string.Format("https://www.youtube.com/watch?v={0}", match.Videos[0].Key));
         }
     }
 }
