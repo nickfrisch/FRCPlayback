756849074cb8a5974af9134ac0cc97f5f2bd5a67
Wyatt <26737@oxfordwildcats.org>
2023-06-25 23:01:41 -0400

    Fixed Ambiguous "Match" Type in Main class

--- FRC Playback/MainWindow.xaml.cs
+++ FRC Playback/MainWindow.xaml.cs
@@ -1,30 +1,13 @@
 using FRC_Playback.TBAUtils;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using TBAAPI.V3Client.Api;
 using TBAAPI.V3Client.Client;
 using TBAAPI.V3Client.Model;
 using YoutubeExplode;
-using YoutubeExplode.Videos;
 using YoutubeExplode.Videos.Streams;
+using Match = TBAAPI.V3Client.Model.Match;
 
 namespace FRC_Playback
 {
